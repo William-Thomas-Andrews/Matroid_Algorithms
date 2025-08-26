c21b30532e9065eb14de594ea4ca3562d4a4be34
William Andrews <159858862+William-Thomas-Andrews@users.noreply.github.com>
2025-08-26 19:16:39 -0400

    Refactor Oracle class to include constructor and method

--- src/Oracle.hpp
+++ src/Oracle.hpp
@@ -1,8 +1,8 @@
-#pragma once
-
 template <class SET, typename ELEMENT>
 class Oracle {
     public:
-        Oracle();
-        bool independent(SET S, ELEMENT e);
+        Oracle() {}
+        bool independent(SET S, ELEMENT e) {
+            return (S.is_independent(e));
+        }
 };
