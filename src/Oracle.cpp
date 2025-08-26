23f8eead7af0cc60a0c506ee4428caa303828901
William Andrews <159858862+William-Thomas-Andrews@users.noreply.github.com>
2025-08-26 19:16:29 -0400

    Delete src/Oracle.cpp

--- src/Oracle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Oracle.hpp"
-
-template <class SET, typename ELEMENT>
-Oracle<SET, ELEMENT>::Oracle() {}
-
-template <class SET, typename ELEMENT>
-bool independent(SET S, ELEMENT e) {
-    return (S.is_independent(e));
-}
