8ca3facf09f3c2c2ab8e8533d3b9cb85d1e8c202
William Andrews <159858862+William-Thomas-Andrews@users.noreply.github.com>
2025-08-26 19:14:03 -0400

    Implement comparison operators in MinCompare and MaxCompare

--- src/utils/Compare.hpp
+++ src/utils/Compare.hpp
@@ -1,13 +1,17 @@
-#pragma once
+#include <iomanip>
 
 template <typename ELEMENT>
 class MinCompare {
     public:
-        bool operator()(ELEMENT a, ELEMENT b);
+        bool operator()(ELEMENT a, ELEMENT b) {
+            return (a > b);
+        }
 };
 
 template <typename ELEMENT>
 class MaxCompare {
     public:
-        bool operator()(ELEMENT a, ELEMENT b);
+        bool operator()(ELEMENT a, ELEMENT b) {
+            return (a < b);
+        }
 };
