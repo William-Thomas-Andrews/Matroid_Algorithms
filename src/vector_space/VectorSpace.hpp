6d69767c0b521a85b3f9ccd577902b0c8abde13b
William Andrews <159858862+William-Thomas-Andrews@users.noreply.github.com>
2025-08-26 19:15:58 -0400

    Refactor VectorSpace constructor and methods

--- src/vector_space/VectorSpace.hpp
+++ src/vector_space/VectorSpace.hpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include <iostream>
 #include "Matrix.hpp"
 
@@ -11,18 +9,41 @@ class VectorSpace {
         std::vector<std::unique_ptr<Matrix>> matrix_collection;
 
     public:
-        VectorSpace();
-        VectorSpace(int input_dimension);
+        VectorSpace() : dimension(0) {
+            standard_basis = Matrix(0, 1, 1);
+        }
+        VectorSpace(int input_dimension) : dimension(input_dimension) {
+            standard_basis = Matrix(0, input_dimension, input_dimension);
+            for (int i = 0, j = 0; i < dimension; i++, j++) {
+                standard_basis(i, j) = 1;
+            }
+        }
 
-        void add_matrix(Matrix& A);
+        void add_matrix(Matrix& A) {
+            if (A.get_rows() > dimension) { throw std::invalid_argument("The input matrix cannot have a larger dimension than the vector space"); }
+            std::unique_ptr<Matrix> ptr = std::make_unique<Matrix>(A);
+            matrix_collection.push_back(std::move(ptr));
+        }
 
-        void print_standard_basis();
+        void print_standard_basis() {
+            std::cout << standard_basis << std::endl;
+        }
 
-        Matrix& get_matrix(int index);
+        Matrix& get_matrix(int index) {
+            if (index >= dimension or index < 0) { throw std::invalid_argument("Index out of bounds"); }
+            return *matrix_collection[index];
+        }
 
-        bool check_linear_independence(Matrix& A, Vector& v);
+        bool check_linear_independence(Matrix& A, Vector& v) {
+            if (A.is_independent(v)) {
+                return true;
+            }
+            return false;
+        }
 
         friend int dim(const VectorSpace& V);
 };
 
-int dim(const VectorSpace& V);
+int dim(const VectorSpace& V) {
+    return V.dimension;
+}
